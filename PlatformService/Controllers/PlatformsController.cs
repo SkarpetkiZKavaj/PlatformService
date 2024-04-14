277148f995f3f08edc986ed9877983417142ce53
SkarpetkiZKavaj <wasilewski.ilia@gmail.com>
2024-04-14 22:34:27 +0200

    feat: implement add base controllers logic and implement GetAll endpoint

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data.Dtos;
+using PlatformService.Data.Interfaces;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepository _repository;
+    private readonly IMapper _mapper;
+    
+    public PlatformsController(IPlatformRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        var platforms = _repository.GetAll();
+
+        return Ok(_mapper.Map<IEnumerable<Platform>, IEnumerable<PlatformReadDto>>(platforms));
+    }
+}
